7ae18d0091636dff57356ca82782a7ee6f2c6ae7
Ruben Fonte <ruben.m.fonte@gmail.com>
2021-03-13 11:49:17 +0000

    Changed to subtraction

--- teste/src/main/java/teste/teste/App.java
+++ teste/src/main/java/teste/teste/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
     }
     
-    public static int addition(int a, int b) {
-    	return a + b;
+    public static int subtraction(int a, int b) {
+    	return a - b;
     }
 }
