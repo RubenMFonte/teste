08215b4b5cdfd816be6ca653f25ed9533071b1ca
crlss-iscte <crlss@iscte-iul.pt>
2021-03-15 21:56:07 +0000

    funcao dividir

--- teste/src/main/java/teste/teste/App.java
+++ teste/src/main/java/teste/teste/App.java
@@ -9,8 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("dividir");
     }
-    
+   public static int dividir(int a, int b) {
+	   return (a/b);
+	   
+   }
     public static int subtraction(int a, int b) {
     	return a - b;
     }
