b47446054624e32d839ece5e19a8064c561fbc08
crlss-iscte <crlss@iscte-iul.pt>
2021-03-15 22:31:22 +0000

    pls

--- teste/src/main/java/teste/teste/App.java
+++ teste/src/main/java/teste/teste/App.java
@@ -11,21 +11,13 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("dividir");
     }
-<<<<<<< HEAD
-    
-    public static int multiplication(int a, int b) {
-    	return a * b;
-    }
-    
-    public static int danielLindo(int rei, int delas) {
-    	return rei + delas;
-=======
    public static int dividir(int a, int b) {
 	   return (a/b);
 	   
    }
     public static int subtraction(int a, int b) {
     	return a - b;
->>>>>>> refs/heads/Dividir
     }
+
+    
 }
