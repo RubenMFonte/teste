8b736dd00f64e5f9442355cf36fc41a3c0d593d0
Ruben Fonte <ruben.m.fonte@gmail.com>
2021-03-15 22:45:43 +0000

    added power function

--- teste/src/main/java/teste/teste/App.java
+++ teste/src/main/java/teste/teste/App.java
@@ -14,4 +14,9 @@ public class App
     public static int subtraction(int a, int b) {
     	return a - b;
     }
+    
+    public static int power(int a, int b){
+    	if( b == 1 ) return a;
+    	else return a * power(a, b-1);
+    }
 }
